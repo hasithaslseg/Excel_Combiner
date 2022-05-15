55297dcda12d1c05e884f8ed8b43f14c08b3e2f4
Hasitha Sampath Kulappu Arachchige <81281005+hasithaslseg@users.noreply.github.com>
2022-05-15 08:51:30 +0530

    Update excel_creater.py

--- excel_creater.py
+++ excel_creater.py
@@ -1,11 +1,12 @@
 import os
 import excel_writer
 
-#/* there are three imput variables to the excel_writer.row_writer method
+# there are three input variables to the excel_writer.row_writer method
 """
 input variable 1: Enter the name of the Excel Work Book you are going to create
-input variable 2: Enter the name of the excel sheet
-input variable 3: This will input the names of the excel files in the specified folder that you are going to merge as a List
+input variable 2: Enter the name of the excel sheet you are going to create
+input variable 3: This will input the names of the excel files in the specified folder that you are going to merge as a List which is generated from the listdir function
+when you input the path of the directory
 """
 
 b = (os.listdir(r'C:\Users\Acer\PycharmProjects\pythonProject\CV'))
